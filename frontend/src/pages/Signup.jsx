dfbb6774fcf101f6aa7bb519d1d4f9467ddffa72
apricity2u <gkdms9849@gmail.com>
2025-02-25 07:59:00 +0900

    [BK-23] [feat] 회원가입 폼 제출

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import authApi from '../api/authApi';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const navigate = useNavigate();
+
   // 회원가입 폼 데이터 제출용 state
   const [userData, setUserData] = useState({
     username: '',
@@ -13,26 +16,34 @@ export default function Signup() {
   // 아이디 중복확인용 state
   const [isUsed, setIsUsed] = useState(false);
 
-  // 아이디 중복확인용 api
-  async function handleDuplicatedUsername() {
+  // 아이디 중복확인용 함수
+  async function handleUsedUsername() {
     try {
       const response = await authApi.verifyUsername(username);
       const data = response.data;
       setIsUsed(data.used);
     } catch (e) {
-      e.message('입력값을 확인해주세요');
+      console.error('입력값을 확인해주세요');
     }
   }
 
-  // 회원가입 폼 제출
-  function handleSubmit(e) {
+  // 회원가입 폼 제출용 함수
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log(userData);
+
+    try {
+      await authApi.signup(userData);
+      navigate('/login');
+    } catch {
+      console.error('입력값을 확인해주세요');
+    }
   }
 
   // 회원가입 폼 입력값
   function handleFormInput(e) {
     const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
     setUserData((prev) => ({ ...prev, [name]: value }));
   }
 
@@ -49,7 +60,7 @@ export default function Signup() {
             value={userData.username}
             onChange={handleFormInput}
           />
-          <button onClick={handleDuplicatedUsername}>중복확인</button>
+          <button onClick={handleUsedUsername}>중복확인</button>
           {isUsed && <div>이미 사용중인 아이디입니다.</div>}
         </div>
         <label htmlFor="password">비밀번호</label>
@@ -66,9 +77,9 @@ export default function Signup() {
         <input type="text" name="name" id="name" value={userData.name} onChange={handleFormInput} />
         <label htmlFor="phone1">전화번호</label>
         <div>
-          <input type="tel" id="phone1" name="phoneNumber" /> -
-          <input type="tel" id="phone2" name="phoneNumber" /> -
-          <input type="tel" id="phone3" name="phoneNumber" />
+          <input type="tel" id="phone1" name="phone1" /> -
+          <input type="tel" id="phone2" name="phone2" /> -
+          <input type="tel" id="phone3" name="phone3" />
         </div>
         <button>회원가입</button>
       </form>
