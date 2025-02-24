c7a6f7f3327e7876e40b0ae2a5ce4f9f06353851
cocobabb <cocobab05108@gmail.com>
2025-02-25 01:07:53 +0900

    [BK-17] [feat] Axios를 이용한 API 통신 기능 추가

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,65 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export default function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: '',
+    setPassword: '',
+  });
+
+  const handleFormInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  let blankCheck = false;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await authApi.login(formData);
+      const data = response.data;
+
+      const { token } = data.data;
+      navigate('/');
+    } catch {
+      console.error();
+    }
+  };
+
   return (
     <>
       <div>Login</div>
       <label htmlFor="logo">
         <h1>
-          <input type="button" id="logo"/>
+          <input type="button" id="logo" />
           로고
         </h1>
       </label>
-      <form action="">
-        <input name="username" type="text" />
-        <div className="blankCheck">필수 입력 값입니다</div>
-        <input name="password" type="password" />
-        <div className="blankCheck">필수 입력 값입니다</div>
-        <div>아이디와 비밀번호를 확인해주세요</div>
+      <form action="" name="formData" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          placeholder="아이디"
+          onChange={handleFormInput}
+        />
+        {blankCheck && <div>필수 입력 값입니다</div>}
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="비밀번호"
+          onChange={handleFormInput}
+        />
+        {blankCheck && <div>필수 입력 값입니다</div>}
+        <br />
+        {/* <div>아이디와 비밀번호를 확인해주세요</div> */}
         <button>로그인</button>
       </form>
 
