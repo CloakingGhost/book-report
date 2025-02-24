0410372d71a8ce6b326b45dc81503e63ddae504d
JaeHyeon_Lee <project0858@gmail.com>
2025-02-24 14:33:47 +0900

    [BK-19] [chore] 시큐리티 기본 설정 완료 폴더 구조와, 시큐리티 설정 - api/** 으로 모든 경로 열어 두었음 - 비밀번호 인코딩 클래스 빈으로 등록 - 생성일, 수정일 공통(common) 엔티티에 있음 - 권한 1개만 존재 (ROLE_USER)
    Co-authored-by: CloakingGhost <project0858@gmail.com>

--- backend/src/main/java/org/example/book_report/controller/AuthController.java
+++ backend/src/main/java/org/example/book_report/controller/AuthController.java
@@ -2,6 +2,7 @@ package org.example.book_report.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.book_report.service.AuthService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 
 
 }
