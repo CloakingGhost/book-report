32b39ef265cc5339fc0ddc57d6dbbf4cc34a2978
apricity2u <gkdms9849@gmail.com>
2025-02-25 07:39:42 +0900

    [BK-23] [refactor] 회원가입 폼 입력값 받기

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import authApi from '../api/authApi';
 
 export default function Signup() {
-  const [username, setUsername] = useState('');
+  // 회원가입 폼 데이터 제출용 state
+  const [userData, setUserData] = useState({
+    username: '',
+    password: '',
+    name: '',
+    phoneNumber: '',
+  });
+
   // 아이디 중복확인용 state
   const [isUsed, setIsUsed] = useState(false);
 
@@ -17,31 +24,46 @@ export default function Signup() {
     }
   }
 
-  function handleInputUsername(e) {
-    setUsername(e.target.value);
+  // 회원가입 폼 제출
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(userData);
   }
+
+  // 회원가입 폼 입력값
+  function handleFormInput(e) {
+    const { name, value } = e.target;
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  }
+
   return (
     <>
       <h1>로고</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">아이디</label>
         <div>
           <input
             type="text"
             name="username"
             id="username"
-            value={username}
-            onChange={handleInputUsername}
+            value={userData.username}
+            onChange={handleFormInput}
           />
           <button onClick={handleDuplicatedUsername}>중복확인</button>
           {isUsed && <div>이미 사용중인 아이디입니다.</div>}
         </div>
         <label htmlFor="password">비밀번호</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={userData.password}
+          onChange={handleFormInput}
+        />
         <label htmlFor="checkPassword">비밀번호 확인</label>
         <input type="password" name="checkPassword" id="checkPassword" />
         <label htmlFor="name">이름</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" value={userData.name} onChange={handleFormInput} />
         <label htmlFor="phone1">전화번호</label>
         <div>
           <input type="tel" id="phone1" name="phoneNumber" /> -
