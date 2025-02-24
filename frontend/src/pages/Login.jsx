4ba34e85d7ca6522eeb5230c0f43182b562d3e30
cocobabb <cocobab05108@gmail.com>
2025-02-24 20:36:10 +0900

    [BK-17] [feat] 로그인 기본 구조 잡기

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
-
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div>Login</div>
+      <label htmlFor="logo">
+        <h1>
+          <input type="button" id="logo"/>
+          로고
+        </h1>
+      </label>
+      <form action="">
+        <input name="username" type="text" />
+        <div className="blankCheck">필수 입력 값입니다</div>
+        <input name="password" type="password" />
+        <div className="blankCheck">필수 입력 값입니다</div>
+        <div>아이디와 비밀번호를 확인해주세요</div>
+        <button>로그인</button>
+      </form>
+
+      <div>
+        아직 계정이 없으신가요? <button>회원가입</button>
+      </div>
+    </>
+  );
 }
