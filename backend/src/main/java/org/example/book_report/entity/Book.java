cd19daadda6d6bb9527ac84110563ddfc477f2b3
ysc13245 <ysc13245@gmail.com>
2025-02-25 20:44:06 +0900

    [BK-47] [feat] 도서 검색 기능 구현 Book User 엔티티 연관관계 맺고 FindAllByTitle 페이지네이션 함

--- backend/src/main/java/org/example/book_report/entity/Book.java
+++ backend/src/main/java/org/example/book_report/entity/Book.java
@@ -1,6 +1,7 @@
 package org.example.book_report.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,15 +10,16 @@ import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
 public class Book {
     // TODO: 제약조건 추가
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name="id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private User user;
     private String image; // TODO: 이미지 엔티티 생성 후 교체
     private String title;
--- backend/src/main/java/org/example/book_report/service/BookRepository.java
+++
@@ -1,4 +0,0 @@
-package org.example.book_report.service;
-
-public interface BookRepository {
-}
