ff495b0f2406782bf176d933d5c851bc3c03158d
cocobabb <cocobab05108@gmail.com>
2025-02-26 01:33:45 +0900

    [BK-50] [feat] 감상문 작성의 기본 구조, 도서 검색 axios연결

--- frontend/src/pages/BookReview.jsx
+++ frontend/src/pages/BookReview.jsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import bookApi from '../api/bookApi';
 
 export default function BookReview() {
-  return <div>BookReview</div>;
+  const [searchBook, setSearchBook] = useState('');
+  const [bookImage, setBookImage] = useState('');
+  const [cardImage, setCardImage] = useState('');
+
+  // 도서 검색 관련
+  const [status, setStatus] = useState();
+  const [page, setPage] = useState();
+  const [size, setSize] = useState();
+  const [items, setItems] = useState([]);
+
+  const handleSearchBookTitle = async (e) => {
+    setSearchBook(e.target.value);
+
+    try {
+      const response = await bookApi.searchBooks(searchBook);
+      const [status, page, size, items] = response;
+      setItems(items);
+    } catch {
+      console.error();
+    }
+  };
+
+  useEffect(() => {
+    console.log(searchBook);
+  }, [searchBook]);
+
+  return (
+    <>
+      <input type="text" onChange={handleSearchBookTitle} />
+
+      <img name="bookImage" src={bookImage} alt="" />
+      <input name="title" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+      <input name="author" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+      <input name="publisher" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+
+      <img name="cardImage" src={cardImage} alt="" />
+      <input name="onelineTitle" type="text" />
+
+      <textarea name="" id=""></textarea>
+    </>
+  );
 }
