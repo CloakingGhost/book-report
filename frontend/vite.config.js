b0fa680efe059f413f9321fe20930c9239d29402
apricity2u <gkdms9849@gmail.com>
2025-02-25 17:27:37 +0900

    [BK-41] [chore] 개발용 프록시 서버 재추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
-// Gzip 모듈 불러오기
-import compression from 'vite-plugin-compression';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // compression() 추가
     compression({
-      algorithm: 'gzip', // Gzip 알고리즘 사용
-      ext: '.gz', // 생성되는 파일의 확장자
-      threshold: 10240, // 압축 최소 크기 (10KB 이상만 압축)
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 1024,
+      deleteOriginFile: false,
     }),
   ],
+
+  // 개발서버 때는 사용되지 않음
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
