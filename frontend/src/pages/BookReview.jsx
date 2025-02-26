d758cfca2bd564eade4212149ea586de12498fba
cocobabb <cocobab05108@gmail.com>
2025-02-26 18:13:35 +0900

    [BK-33] [feat] 도서검색 모달창과 정보 자동입력 트리거 완성

--- frontend/src/pages/BookReview.jsx
+++ frontend/src/pages/BookReview.jsx
@@ -2,51 +2,93 @@ import React, { useEffect, useState } from 'react';
 import bookApi from '../api/bookApi';
 
 export default function BookReview() {
-  const [searchBook, setSearchBook] = useState('');
+  // 도서 검색 관련
+  const [searchBook, setSearchBook] = useState(false);
+  const [bookItems, setBookItems] = useState([]);
+
+  // 도서 정보 입력칸 관련
   const [bookImage, setBookImage] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publisher, setPublisher] = useState('');
+
   const [cardImage, setCardImage] = useState('');
 
-  // 도서 검색 관련
-  const [bookList, setBookList] = useState([]);
-
   const handleSearchBookTitle = async (e) => {
-    setSearchBook(e.target.value);
+    const searchTitle = e.target.value;
 
-    try {
-      const response = await bookApi.searchBooks(searchBook);
-      const { hasNext, bookList } = response;
+    setSearchBook(searchTitle);
 
-      setBookList(bookList);
-    } catch {
-      console.error();
+    if (searchTitle !== '') {
+      try {
+        const response = await bookApi.searchBooks(searchTitle);
+
+        const { hasNext, bookList } = response;
+        console.log(bookList);
+
+        setBookItems(bookList);
+      } catch {
+        console.error();
+      }
+    } else {
+      setBookItems([]);
     }
   };
+
+  const setBookInfo = async (book) => {
+    setBookImage(book.imageUrl);
+    setTitle(book.title);
+    setAuthor(book.author);
+    setPublisher(book.publisher);
+  };
+
   return (
     <>
       <input type="text" onChange={handleSearchBookTitle} />
-      {
-        <>
-          {bookList.length != 0 ? (
-            <ul>
-              {bookList.map((book) => (
-                <li key={book.bookId}>
-                  <img src={book.imgUrl} alt="" />
+
+      {searchBook && (
+        <ul>
+          {bookItems.length != 0 ? (
+            <>
+              {bookItems.map((book) => (
+                <li key={book.bookId} onClick={() => setBookInfo(book)}>
+                  <img src={book.imageUrl} alt="" />
                   <div>{book.title}</div>
                   <div>{book.author}</div>
                   <div>{book.publisher}</div>
                 </li>
               ))}
-            </ul>
+            </>
           ) : (
-            <div>도서 검색 직접입력 모달창</div>
+            <>
+              <li>직접입력 모달창</li>
+            </>
           )}
-        </>
-      }
+        </ul>
+      )}
 
       <img name="bookImage" src={bookImage} alt="" />
-      <input name="title" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
-      <input name="author" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
-      <input name="publisher" type="text" placeholder="도서 검색을 먼저 하세요" readOnly />
+      <input
+        name="title"
+        value={title}
+        type="text"
+        placeholder="도서 검색을 먼저 하세요"
+        readOnly
+      />
+      <input
+        name="author"
+        value={author}
+        type="text"
+        placeholder="도서 검색을 먼저 하세요"
+        readOnly
+      />
+      <input
+        name="publisher"
+        value={publisher}
+        type="text"
+        placeholder="도서 검색을 먼저 하세요"
+        readOnly
+      />
 
       <img name="cardImage" src={cardImage} alt="" />
       <input name="onelineTitle" type="text" />
