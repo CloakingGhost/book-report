4b037c1be5723d52826e4b09b24202eecd071baf
cocobabb <cocobab05108@gmail.com>
2025-02-27 21:47:47 +0900

    [BK-33] [feat] 수동이미지파일 선택 및 미리보기,작성문 저장

--- frontend/src/pages/BookReview.jsx
+++ frontend/src/pages/BookReview.jsx
@@ -1,20 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import bookApi from '../api/bookApi';
 import { useRef } from 'react';
+import reviewApi from '../api/reviewApi';
+import { useNavigate } from 'react-router-dom';
 
 export default function BookReview() {
+  const navigate = useNavigate();
+
   // 도서 검색 관련
   const [searchBook, setSearchBook] = useState('');
   const [bookItems, setBookItems] = useState([]);
   const cleanSearchTitle = useRef();
 
-  // 자동 입력 도서 정보 입력칸 관련
-  const [bookImage, setBookImage] = useState('');
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [publisher, setPublisher] = useState('');
+  //////////////// 자동 입력 도서 정보 입력칸 관련///////////////////////
+  const [bookId, setBookId] = useState(null);
+
+  // 도서 이미지 미리보기 변수
+  const [bookImage, setBookImage] = useState(null);
+  // axios에 넘겨줄 도서 이미지 변수
+  const [bookImageFile, setBookImageFile] = useState(null);
+
+  const [title, setTitle] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [publisher, setPublisher] = useState(null);
   const [isReadOnly, setIsReadOnly] = useState(true);
 
+  ////////////////카드 이미지//////////////////////////////
+  const [cardImage, setCardImage] = useState();
+
+  // 감상문 내용
+  const [content, setContent] = useState();
+
+  // 도서 검색Api
   const handleSearchBookTitle = async (e) => {
     const searchTitle = e.target.value;
 
@@ -34,17 +51,19 @@ export default function BookReview() {
     }
   };
 
+  // 자동 입력
   const setBookInfo = async (book) => {
-    setSearchBook(cleanSearchTitle.current.value);
     cleanSearchTitle.current.value = '';
+    setSearchBook(cleanSearchTitle.current.value);
     setIsReadOnly(true);
+    setBookId(book.bookId);
     setBookImage(book.imageUrl);
     setTitle(book.title);
     setAuthor(book.author);
     setPublisher(book.publisher);
   };
 
-  // 사용자 직접 입력
+  // 사용자 수동 입력
   const onUserInput = () => {
     setTitle(searchBook);
     cleanSearchTitle.current.value = '';
@@ -54,10 +73,70 @@ export default function BookReview() {
   };
 
   function removeBookInfo() {
-    setBookImage('');
-    setAuthor('');
-    setPublisher('');
+    setBookImage(null);
+    setBookImageFile(null);
+    setAuthor(null);
+    setPublisher(null);
   }
+
+  // 수동 입력 이미지 넣기
+  function addImage(e) {
+    const addImage = e.target.files;
+    console.log(addImage);
+    setBookImageFile(addImage[0]);
+
+    const imageurl = URL.createObjectURL(addImage[0]);
+
+    setBookImage(imageurl);
+  }
+
+  // 감상문 저장
+  const saveBookReview = async () => {
+    console.log(bookImageFile);
+    console.log(title);
+    console.log(author);
+    console.log(publisher);
+    if (bookId == null) {
+      const bookReview = {
+        book: {
+          title: null,
+          author: null,
+          publisher: null,
+          imageFile: bookImageFile, // (책 표지) => 임의로 지정함
+        },
+
+        review: {
+          imageFile: cardImage, // 카드 커버
+          title: '한줄평',
+          content: content,
+        },
+      };
+    } else {
+      const bookReview = {
+        book: {
+          title: title,
+          author: author,
+          publisher: publisher,
+          imageFile: bookImage, // (책 표지) => 임의로 지정함
+        },
+
+        review: {
+          imageFile: cardImage, // 카드 커버
+          title: '한줄평',
+          content: content,
+        },
+      };
+    }
+
+    try {
+      const response = await reviewApi.createReview(bookReview);
+      // const { status, bookReviewId } = response;
+      const bookReviewId = 1;
+      navigate(`/reviews/{bookReviewId}`);
+    } catch {
+      console.error();
+    }
+  };
 
   return (
     <>
@@ -89,14 +168,35 @@ export default function BookReview() {
           )}
         </ul>
       )}
-      <img name="bookImage" src={bookImage} alt="" />
-      {!isReadOnly && <button>이미지 첨부</button>}
+      <img
+        name="bookImage"
+        src={bookImage}
+        alt=""
+        style={{ border: 'black, solid ,1px', width: '500px', height: '500px' }}
+      />
+      {!isReadOnly && (
+        <input type="file" accept="image/png, image/jpeg, image/jpg" onChange={addImage} />
+      )}
       책 제목: <input name="title" defaultValue={title} type="text" readOnly />
-      작가: <input name="author" defaultValue={author} type="text" readOnly={isReadOnly} />
+      작가:
+      <input
+        name="author"
+        defaultValue={author}
+        type="text"
+        readOnly={isReadOnly}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
       출판사:
-      <input name="publisher" defaultValue={publisher} type="text" readOnly={isReadOnly} />
+      <input
+        name="publisher"
+        defaultValue={publisher}
+        type="text"
+        readOnly={isReadOnly}
+        onChange={(e) => setPublisher(e.target.value)}
+      />
       <div>나중에 카드 컴포넌트 들어갈 자리</div>
-      <textarea name="" id=""></textarea>
+      <textarea name="content" onChange={(e) => setContent(e.target.value)}></textarea>
+      <button onClick={saveBookReview}>저장</button>
     </>
   );
 }
