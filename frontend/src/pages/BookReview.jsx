802e105b717dc767207c54812e9d62713cf92d68
cocobabb <cocobab05108@gmail.com>
2025-02-27 14:31:44 +0900

    [BK-33] [feat] 자동입력 로직 수정, 수동입력 트리거 기능완성

--- frontend/src/pages/BookReview.jsx
+++ frontend/src/pages/BookReview.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import bookApi from '../api/bookApi';
+import { useRef } from 'react';
 
 export default function BookReview() {
   // 도서 검색 관련
-  const [searchBook, setSearchBook] = useState(false);
+  const [searchBook, setSearchBook] = useState('');
   const [bookItems, setBookItems] = useState([]);
-  const [isReadOnly, setIsReadOnly] = useState(true);
+  const cleanSearchTitle = useRef();
 
-  // 도서 정보 입력칸 관련
+  // 자동 입력 도서 정보 입력칸 관련
   const [bookImage, setBookImage] = useState('');
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [publisher, setPublisher] = useState('');
-
-  // 한줄평 관련
-  const [cardImage, setCardImage] = useState('');
+  const [isReadOnly, setIsReadOnly] = useState(true);
 
   const handleSearchBookTitle = async (e) => {
     const searchTitle = e.target.value;
@@ -36,37 +35,47 @@ export default function BookReview() {
   };
 
   const setBookInfo = async (book) => {
+    setSearchBook(cleanSearchTitle.current.value);
+    cleanSearchTitle.current.value = '';
+    setIsReadOnly(true);
     setBookImage(book.imageUrl);
     setTitle(book.title);
     setAuthor(book.author);
     setPublisher(book.publisher);
   };
 
-  const onUserInput = (searchBook) => {
-    if (searchBook) {
-      setIsReadOnly(false);
-
-      setBookImage('');
-      setAuthor('');
-      setPublisher('');
-      setTitle(searchBook);
-    }
+  // 사용자 직접 입력
+  const onUserInput = () => {
+    setTitle(searchBook);
+    cleanSearchTitle.current.value = '';
+    setSearchBook(cleanSearchTitle.current.value);
+    removeBookInfo();
+    setIsReadOnly(false);
   };
-  useEffect(() => {
-    console.log(isReadOnly);
-  }, []);
+
+  function removeBookInfo() {
+    setBookImage('');
+    setAuthor('');
+    setPublisher('');
+  }
 
   return (
     <>
-      <input type="text" onChange={handleSearchBookTitle} />
-
-      {searchBook && (
+      검색:{' '}
+      <input
+        type="text"
+        defaultValue={searchBook}
+        ref={cleanSearchTitle}
+        onChange={handleSearchBookTitle}
+      />
+      {searchBook != '' && (
         <ul>
           {bookItems.length != 0 ? (
             <>
               {bookItems.map((book) => (
                 <li key={book.bookId} onClick={() => setBookInfo(book)}>
                   <img src={book.imageUrl} alt="" />
+                  <img src="" alt="" />
                   <div>{book.title}</div>
                   <div>{book.author}</div>
                   <div>{book.publisher}</div>
@@ -75,38 +84,18 @@ export default function BookReview() {
             </>
           ) : (
             <>
-              <li onClick={() => onUserInput(searchBook)}>"{searchBook}" 직접입력</li>
+              <li onClick={onUserInput}>"{searchBook}" 직접 입력</li>
             </>
           )}
         </ul>
       )}
-
       <img name="bookImage" src={bookImage} alt="" />
-      <input
-        name="title"
-        value={title}
-        type="text"
-        placeholder="도서 검색을 먼저 하세요"
-        readOnly={isReadOnly}
-      />
-      <input
-        name="author"
-        value={author}
-        type="text"
-        placeholder="도서 검색을 먼저 하세요"
-        readOnly={isReadOnly}
-      />
-      <input
-        name="publisher"
-        value={publisher}
-        type="text"
-        placeholder="도서 검색을 먼저 하세요"
-        readOnly={isReadOnly}
-      />
-
-      <img name="cardImage" src={cardImage} alt="" />
-      <input name="onelineTitle" type="text" />
-
+      {!isReadOnly && <button>이미지 첨부</button>}
+      책 제목: <input name="title" defaultValue={title} type="text" readOnly />
+      작가: <input name="author" defaultValue={author} type="text" readOnly={isReadOnly} />
+      출판사:
+      <input name="publisher" defaultValue={publisher} type="text" readOnly={isReadOnly} />
+      <div>나중에 카드 컴포넌트 들어갈 자리</div>
       <textarea name="" id=""></textarea>
     </>
   );
