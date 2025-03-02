877ad0cc356779d7d2f1c3669c5c0f8e4ee0fd08
ysc <ysc13245@gmail.com>
2025-03-03 04:20:26 +0900

    [BK-91] [fix] 감상문작성 매개변수 관련 수정 책표지 이미지 없을 때 {imageFile:none} 없이 data만 보내도 처리되도록 로직 수정

--- backend/src/main/java/org/example/book_report/service/BookReviewService.java
+++ backend/src/main/java/org/example/book_report/service/BookReviewService.java
@@ -50,9 +50,12 @@ public class BookReviewService {
     public CreateReviewResponseDto createReview(CreateReviewRequestDto createReviewRequestDto,
                                                 MultipartFile imageFile, User user) {
 
-        Image image = imageService.uploadImage(imageFile, user);
+        Image image = null;
         Book book;
+        if (imageFile != null && !imageFile.isEmpty()) {
 
+            image = imageService.uploadImage(imageFile, user);
+        }
 
         if (createReviewRequestDto.getBook().getBookId() == null) {
             book = bookRepository.save(createReviewRequestDto.getBook().toEntity(image, user));
