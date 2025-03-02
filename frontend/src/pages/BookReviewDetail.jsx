572a1403fd4731af6f294f2ab474464ed35b4671
apricity2u <gkdms9849@gmail.com>
2025-03-02 17:39:41 +0900

    [BK-87] [style] 감상문 상세 내용 구도 잡기

--- frontend/src/pages/BookReviewDetail.jsx
+++ frontend/src/pages/BookReviewDetail.jsx
@@ -18,22 +18,26 @@ export default function BookReviewDetail() {
           <div>작가 | 출판사</div>
         </div>
       </section>
+      <div className={styles.dividedLine}></div>
       <section className={styles.bookReviewSection}>
         <section>
           <Link to={'/userpage/:username'}>
-            <h1>유저 아이디</h1>
+            <h1>apricity2u</h1>
           </Link>
-          <div>작성날짜</div>
-          <div>모달 버튼</div>
-          <div>
-            <div>감상문 수정</div>
-            <div>감상문 삭제</div>
-            <div>감상문 비공개/공개</div>
+          <div className={styles.postUserDetail}>
+            <div>최초 작성일 2025.04.04</div>
+            <div>🔒🔓</div>
+            <div>•••</div>
+            {/* <div className={styles.manageReviewSection}>
+              <div>수정</div>
+              <hr />
+              <div>삭제</div>
+              <hr />
+              <div>비공개 | 공개</div>
+            </div> */}
           </div>
-          <div>자물쇠</div>
         </section>
-        <article>
-          <h2>감상문</h2>
+        <article className={styles.bookReview}>
           <h3>한줄평</h3>
           <p>내용</p>
         </article>
