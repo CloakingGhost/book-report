7c8a8ea1a97e275b834eba433c8e32d15b6eadf4
apricity2u <gkdms9849@gmail.com>
2025-03-02 11:20:23 +0900

    [BK-87] [feat] 감상문 상세페이지 초기 구조 설계

--- frontend/src/pages/BookReivewDetatil.jsx
+++ frontend/src/pages/BookReivewDetatil.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function S() {
+export default function BookReivewDetatil() {
   return (
-    <div>S</div>
-  )
+    <main>
+      <section>
+        <h1>책 정보</h1>
+        <div>
+          <img src="" alt="" />
+          <div>인증마크</div>
+        </div>
+        <h2>제목</h2>
+        <div>작가</div>
+        <div>출판사</div>
+      </section>
+      <section>
+        <section>
+          <Link to={'/userpage/:username'}>
+            <h1>유저 아이디</h1>
+          </Link>
+          <div>작성날짜</div>
+          <div>모달 버튼</div>
+          <div>
+            <div>감상문 수정</div>
+            <div>감상문 삭제</div>
+            <div>감상문 비공개/공개</div>
+          </div>
+          <div>자물쇠</div>
+        </section>
+        <article>
+          <h2>감상문</h2>
+          <h3>한줄평</h3>
+          <p>내용</p>
+        </article>
+      </section>
+    </main>
+  );
 }
