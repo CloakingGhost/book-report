8b2964ecd93f20887f6da0d78762c355cdd2b5e8
ysc <ysc13245@gmail.com>
2025-03-02 19:29:41 +0900

    [BK-90] [feat] 네이버 도서검색 api

---
+++ etc/book-scrap/api.py
@@ -0,0 +1,36 @@
+from requests import get
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def fetch_api(query):
+
+    url = os.getenv("NAVER_API_URL")
+    cur = -99
+    PAGE = 100
+    CLIENT_ID = os.getenv("NAVER_CLIENT_ID")
+    CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
+    total = 10000
+
+    while cur + PAGE < total:
+        cur += PAGE
+        headers = {"X-Naver-Client-Id": CLIENT_ID, "X-Naver-Client-Secret": CLIENT_SECRET}
+        params = {"query": query, "display": 100, "start": cur}
+
+        response = get(url=url, headers=headers, params=params)
+        data = response.json()
+        books = [
+            {
+                "member_id": -1,
+                "title": li["title"].strip(),
+                "author": li["author"].strip(),
+                "publisher": li["publisher"].strip(),
+            }
+            for li in data["items"]
+        ]
+        images = [
+            {"type": "BOOK", "image_url": li["image"].strip()} for li in data["items"]
+        ]
+
+        return {"books": books, "images": images}
