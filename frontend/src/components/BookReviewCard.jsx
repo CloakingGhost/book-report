3adeca68eab8612bd62b821e5cd46ef6d57a000b
JaeHyeon_Lee <project0858@gmail.com>
2025-03-03 18:42:44 +0900

    [BK-28] [feat] 메인화면 감상문 조회

--- frontend/src/components/BookReviewCard.jsx
+++ frontend/src/components/BookReviewCard.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import style from '../styles/BookReviewCard.module.css';
 import { Link } from 'react-router-dom';
-Link
-function BookReviewCard({ bookReviewId, title, imageUrl, approved }) {
+
+function BookReviewCard({ info }) {
+  console.log('BookReviewCard', info);
+  const { bookReviewId, title, imageUrl, approved } = info;
   return (
-    <div className={style.bookReviewCard}>
-      <p>{title.length > 20 ? title.substring(0, 20) + '...' : title}</p>
-    </div>
+    <Link to={`/reviews/${bookReviewId}`}>
+
+      <div className={style.bookReviewCard}>
+        <div className={style.imageContainer}>
+          <img src={imageUrl} alt={title} />
+          <p>{title.length > 20 ? title.substring(0, 20) + '...' : title}</p>
+        </div>
+        {!approved && 비공개}
+      </div>
+
+
+    </Link>
   );
 };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import BookReviewCard from '../components/BookReviewCard';
 import styles from '../styles/Home.module.css';
 import reviewApi from '../api/reviewApi';
 
 export default function Home() {
+  /**
+   * 감상문 목록
+   */
   const [items, setItems] = useState([]);
+  /**
+   * 검색어
+   */
   const [title, setTitle] = useState('');
+  /**
+   * 현재 페이지
+   */
   const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchItems(page, title);
   }, [page, title]);
 
+  const handleInput = (e) => {
+    setTitle(e.target.value);
+    setItems([]);
+    setPage(1);
+  }
   const fetchItems = async (page, title) => {
     // API 호출을 통해 데이터를 가져옵니다.
     const response = await reviewApi.getReviews(page, title);
-    const data = response.items;
+    const data = response.items; // 원래 코드
+    console.log(data);
+
     setItems((prevItems) => [...prevItems, ...data]);
   };
 
@@ -32,10 +47,25 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      {items.map((item, index) => (
-        // TODO 컴포넌트에 props로 데이터를 전달합니다. 
-        <BookReviewCard key={card - { index }} info={item} />
-      ))}
+      <div className={styles.searchBox}>
+        <input
+          type="search"
+          value={title}
+          onChange={handleInput}
+          placeholder="검색어를 입력하세요." />
+      </div>
+      <div className={styles.main}>
+        {/* 타이틀이 존재 하면서 응답 값이 있을 때 => 타이틀이 포함 된 카드 렌더링
+          타이틀이 존재 하면서 응답 값이 없을 때 => 검색 결과가 없습니다.
+          타이틀이 없을 때 => ''이 포함된 카드 렌더링(모든 카드 렌더링)
+        */}
+
+        {title && items.length ?
+          items.filter(item => item.approved).map((item, index) => (
+            <BookReviewCard key={`card-${index}`} info={item} />
+          ))
+          : <p>검색 결과가 없습니다.</p>}
+      </div>
     </div>
   );
 }
