eae650a8081cbf1a54349c04462ace153f003200
JaeHyeon_Lee <project0858@gmail.com>
2025-03-03 19:27:17 +0900

    [BK-28] [style] 파일 위치 변경

--- frontend/src/components/card/BookReviewCard.jsx
+++ frontend/src/components/card/BookReviewCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from '../styles/BookReviewCard.module.css';
+import style from '../../styles/BookReviewCard.module.css';
 import { Link } from 'react-router-dom';
 
 function BookReviewCard({ info }) {
