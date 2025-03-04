4c61c22cea78cc75149a444f1869634881543e98
JaeHyeon_Lee <project0858@gmail.com>
2025-03-04 21:00:15 +0900

    [BK-107] [feat] 메인페이지 무한스크롤

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import BookReviewCard from '../components/card/BookReviewCard';
 import styles from '../styles/Home.module.css';
 import reviewApi from '../api/reviewApi';
@@ -6,50 +6,85 @@ import reviewApi from '../api/reviewApi';
 export default function Home() {
   /** 감상문 목록 */
   const [items, setItems] = useState([]);
+  /** back에 요청할 페이지 데이터 순서 정보 */
+  const [offset, setOffset] = useState(0);
+  /** 관찰대상 target */
+  const [target, setTarget] = useState(null);
+  /** Load 중인가를 판별하는 boolean */
+  const [isLoaded, setIsLoaded] = useState(false);
+  /** 마지막 데이터까지 다 불러온 경우 더이상 요청을 보내지 않기 위해서
+      마지막 부분까지 가버릴 때 계속 요청을 보내는 것 방지 */
+  const [stop, setStop] = useState(false);
   /** 검색어 */
   const [title, setTitle] = useState('');
-  /** 현재 페이지 */
-  const [page, setPage] = useState(0);
-
-  useEffect(() => {
-    fetchItems(page, title);
-  }, [page, title]);
 
   const handleInput = (e) => {
     setTitle(e.target.value);
-    setItems([]);
-    setPage(0);
-  }
-  const fetchItems = async (page, title) => {
-    // API 호출을 통해 데이터를 가져옵니다.
-    const response = await reviewApi.getReviews(page, title);
-    const data = response.items; // 원래 코드
-
-    // 공개 데이터만 필터링
-    const filteredData = data.filter((item) => item.approved === true);
-    // 랜덤 정렬
-    const suffledData = filteredData.sort(() => Math.random() - 0.5);
-
-    // 데이터 확인
-    console.log(suffledData);
-
-    setItems((prevItems) => [...prevItems, ...suffledData]);
-  };
-
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-    setPage((prevPage) => prevPage + 1);
+    setItems([]); // 기존 데이터 초기화
+    setOffset(0); // 새 검색어에 대해 처음부터 로드
+    setStop(false); // 새로운 검색이므로 stop 해제
+    setIsLoaded(true); // 새로운 데이터 요청 트리거
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    let observer;
+    if (target && !stop) {
+      // callback 함수로 onIntersect를 지정
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 1,
+      });
+      observer.observe(target);
+    }
+    return () => observer && observer.disconnect();
+  }, [target]);
 
-  const result = items.map((item, index) => (
-    <BookReviewCard key={`card-${index}`} info={item} />
-  ))
-  
+  // isLoaded가 변할 때 실행
+  useEffect(() => {
+    fetchItems(true);
+  }, [title, isLoaded]);
+
+
+  // isLoaded가 true일 때 && 마지막 페이지가 아닌 경우 => 요청보내기
+  const fetchItems = async (reset = false) => {
+    if (isLoaded && !stop) {
+      const response = await reviewApi.getReviews(reset ? 0 : offset, title); // 여기에 offset을 넣어서 요청을 보내야함, 어떻게??
+    // 스프링만 바꾸면 됨 -> offset을 받아서 처리하도록
+      const data = response.items;
+
+      // 공개 데이터만 필터링
+      const filteredData = data.filter((item) => item.approved === true);
+      // 랜덤 정렬
+      const shuffledData = filteredData.sort(() => Math.random() - 0.5);
+      // 받아온 데이터를 보여줄 전체 리스트에 concat으로 넣어준다
+      setItems((prevItems) => (reset ? shuffledData : [...prevItems, ...shuffledData]));
+      // 다음 요청할 데이터 offset 정보
+      setOffset((prevOffset) => (reset ? data.length : prevOffset + data.length));
+      // 다음 요청 전까지 요청 그만 보내도록 false로 변경
+      setIsLoaded(false);
+
+      if (data.length < 12) {
+        // 전체 데이터를 다 불러온 경우(불러온 값이 12개 보다 적다면 -> 매번 12개씩 불러오기로 했으므로 해당 값보다 작으면 마지막 페이지) 아예 로드를 중지
+        setStop(true);
+      }
+    }
+  };
+
+
+  // callback
+  const onIntersect = async ([entry], observer) => {
+    // entry 요소가 교차되거나 Load중이 아니면
+    if (entry.isIntersecting && !isLoaded) {
+      // 관찰은 일단 멈추고
+      observer.unobserve(entry.target);
+      // 데이터 불러오기
+      // 데이터를 받아오도록 true 로 변경
+      setIsLoaded(true);
+
+      // 불러온 후 다시 관찰 실행
+      observer.observe(entry.target);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.searchBox}>
@@ -59,22 +94,21 @@ export default function Home() {
           onChange={handleInput}
           placeholder="검색어를 입력하세요." />
       </div>
-      <div className={styles.main}>
-        {/* 타이틀이 존재 하면서 응답 값이 있을 때 => 타이틀이 포함 된 카드 렌더링
+      {/* 타이틀이 존재 하면서 응답 값이 있을 때 => 타이틀이 포함 된 카드 렌더링
           타이틀이 존재 하면서 응답 값이 없을 때 => 검색 결과가 없습니다.
           타이틀이 없을 때 => ''이 포함된 카드 렌더링(모든 카드 렌더링)
         */}
+      <div className={styles.main}>
         {title ? (
           items.length ? (
-            result
+            items.map((item, index) => <BookReviewCard key={`card-${index}`} info={item} />)
           ) : (
             <p>검색 결과가 없습니다.</p>
           )
         ) : (
-          result
+          items.map((item, index) => <BookReviewCard key={`card-${index}`} info={item} />)
         )}
-        <br />
-        {result}
+        <div ref={setTarget}></div>
 
       </div>
     </div>
